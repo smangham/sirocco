20336f5ff69c374026c6386801111ac6389f2489
jhmatthews <jimim@hotmail.co.uk>
2014-05-02 13:33:39 +0100

    Added test of python run for travis, in folder test_suite

---
+++ test_suite/travis/check_travis_test.py
@@ -0,0 +1,34 @@
+'''
+run simple test of python- check it runs and read sin atomic data
+'''
+
+import os
+import numpy as np 
+
+
+f = open("test2.out")
+
+nerror = 0
+
+for line in f:
+	data = line.split()
+
+	if len(data) > 0:
+		if data[0] == "Get_atomic_data:":
+			if data[1] == "Could" and data[2] == "not":
+				nerror += 1
+				print "Error: %s" % line
+
+		elif data[0] == "Error":
+			nerror += 1
+			print "Error: %s" % line
+	
+
+
+if nerror > 0:
+	print "Errors when running"
+	exit(0)
+
+else:
+	print "Ran python OK"
+	exit(0)		# test was success
